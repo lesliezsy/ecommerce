7c5009db0e0ff95a34557c6d5df47b9cfc43fbfc
Leslie <lesliezsy12@gmail.com>
2020-12-07 11:44:22 +0800

    broken landingpage into smaller components. finished styling menu-item component

---
+++ src/pages/landing/landing.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Directory from '../../components/directory/directory.component';
+import './landing.styles.scss';
+
+const Landing = () => (
+    <div className='landing'>
+        <Directory />
+    </div>
+)
+
+export default Landing;
