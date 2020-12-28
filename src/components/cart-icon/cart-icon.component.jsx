4bd334b843e3843717e985536d3fd8faff27dc3d
Leslie <lesliezsy12@gmail.com>
2020-12-28 11:51:47 +0800

    updating application with reselect, replaced cart selectors with memoized selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 
 // a special syntax in React for importing SVG.
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
@@ -26,10 +27,14 @@ const mapDispatchToProps = dispatch => ({
 })
 
 // 新增cart item quantity
-const mapStateToProps = ({ cart: { cartItems }}) => ({
-    itemCount: cartItems.reduce(
-        (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0
-        )
+const mapStateToProps = state => ({
+    itemCount: selectCartItemsCount(state)
 })
+
+// const mapStateToProps = ({ cart: { cartItems }}) => ({
+//     itemCount: cartItems.reduce(
+//         (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0
+//         )
+// })
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,22 @@
+import { createSelector } from 'reselect';
+
+// 設定selector 讓data可以重複使用：讓shopping cart的資料可以重複利用
+// it's a memorized selector for the value
+
+// 首先從cart-icon傳來state資料，接著帶入cart資料
+const selectCart = state => state.cart;
+
+// 先過取出one layer的資料量，也正好是我們要用的，不用全部資料都帶出
+// 從上方的cart取得cartItems資料
+export const selectCartItems = createSelector(
+    [selectCart],
+    cart => cart.cartItems
+);
+
+// 計算購物車商品數量
+export const selectCartItemsCount = createSelector(
+    [selectCartItems],
+    cartItems => cartItems.reduce(
+        (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0
+        )
+)
