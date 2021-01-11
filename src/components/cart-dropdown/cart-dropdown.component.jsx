d456a4b9333f92b5fc3c41f2556a752556428f87
Leslie <lesliezsy12@gmail.com>
2021-01-11 18:24:12 +0800

    added checkout page, check item component and updated cart to use selectors from reselect

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,10 +6,11 @@ import { withRouter } from "react-router-dom";
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 import { selectCartItems } from "../../redux/cart/cart.selectors";
+import { toggleCartHidden } from '../../redux/cart/cart.actions'
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
             { cartItems.length ? 
@@ -18,14 +19,17 @@ const CartDropdown = ({ cartItems, history }) => (
             ))) : (<span className='empty-message'>Your cart is empty</span>)
         }
         </div>
-        <CustomButton onClick={() => history.push('/checkout')}>CHECKOUT</CustomButton>
+        <CustomButton onClick={() => {
+            history.push('/checkout');
+            dispatch(toggleCartHidden())}
+            }>
+                CHECKOUT
+                </CustomButton>
     </div>
 )
 const mapStateToProps = createStructuredSelector({
     cartItems: selectCartItems
 })
-
-// c 
 
 // const mapStateToProps = ({ cart: { cartItems } }) => ({
 //     cartItems
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -25,3 +25,11 @@ export const selectCartItemsCount = createSelector(
         (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0
         )
 )
+
+// 計算購物車商品總金額
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems => cartItems.reduce(
+        (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity * cartItem.price, 0
+        )
+)
